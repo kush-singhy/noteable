1ab41d2b2dca27ad7d175dc06270602da6645fc0
Kush <153984588+kush-singhy@users.noreply.github.com>
2024-11-03 21:54:42 +1100

    progress

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -4,6 +4,7 @@ import searchIcon from '../assets/search.svg';
 
 function SearchBar() {
     const [input, setInput] = useState('');
+    const [results, setResults] = useState([]);
 
     const handleChange = async (event) => {
         const value = event.target.value;
@@ -14,9 +15,20 @@ function SearchBar() {
         try {
             const response = await axios.post('http://localhost:3000/search', { input });
             console.log(response.data);
+            const searchResults = response.data;
+            setResults(searchResults);
         } catch (err) {
             console.error('Error searching for book:', err);
         }
+    }
+
+    function singleResult(result) {
+      console.log(result);
+      return (
+        <a href="#" class="list-group-item list-group-item-action">
+          <strong>{result.title}</strong> by {result.author}
+        </a>
+      )
     }
 
 
@@ -34,8 +46,15 @@ function SearchBar() {
           <img src={searchIcon} className="search-icon" alt="Search" />
         </button>
       </div>
+      <div className='search-results'>
+        {results.length > 0 &&
+          <ul class="list-group">
+            {results.map(singleResult)};
+          </ul>
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default SearchBar;
